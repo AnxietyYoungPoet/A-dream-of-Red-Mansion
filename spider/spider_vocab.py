9692549ae939a1be57bcf1109fb3bebac9a97ab4
AnxietyYoungPoet <spq1994@hotmail.com>
2018-09-06 17:18:31 +0800

    encoding decoding

--- spider/spider_vocab.py
+++ spider/spider_vocab.py
@@ -5,7 +5,8 @@ text = requests.get(root + '/wordbook/139771/')
 urls = re.findall(r'/wordlist/139771/\d+/', text.text)
 with open('vocabs.txt', 'w') as f:
   for url in urls:
-    text = requests.get(root + url).text
-    words = re.findall(r'<strong>\w+</strong>', text)
-    for word in words:
-      f.write(word[8:-9] + '\n')
+    for index in range(1, 11):
+      text = requests.get(root + url + f'?page={index}').text
+      words = re.findall(r'<strong>\w+</strong>', text)
+      for word in words:
+        f.write(word[8:-9] + '\n')
--- transformation.py
+++ transformation.py
@@ -1,17 +1,42 @@
 import re
-from huffman import huffman_coding
+import pickle
 
 
-def chn2char(text, dcits):
-  with open('texts/original2.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-  s = re.split(r'第\w+回\s+', text)
-  for _ in range(94):
-    s.pop(0)
+def chn2eng(text, chn2eng_dict):
+  encoded_text = ''
+  for word in text:
+    encoded_text = encoded_text + chn2eng_dict[word] + ' '
+  return encoded_text
 
 
-def char2chn(text, dicts):
-  pass
+def eng2chn(text, eng2chn_dict):
+  decoded_text = ''
+  split_text = text.split()
+  for word in split_text:
+    decoded_text = decoded_text + eng2chn_dict[word]
+  return decoded_text
+
+
+def encoding():
+  with open('dicts/chn2eng', 'rb') as f:
+    chn2eng_dict = pickle.load(f)
+  for i in range(1, 121):
+    with open(f'texts/chapter{i}.txt', 'r', encoding='utf-8') as f:
+      text = f.read()
+    encoded_text = chn2eng(text, chn2eng_dict)
+    with open(f'texts/eng_ver/chapter{i}.txt', 'w') as f:
+      f.write(encoded_text)
+
+
+def decoding():
+  with open('dicts/eng2chn', 'rb') as f:
+    eng2chn_dict = pickle.load(f)
+  for i in range(1, 121):
+    with open(f'texts/eng_ver/chapter{i}.txt', 'r') as f:
+      text = f.read()
+    decoded_text = eng2chn(text, eng2chn_dict)
+    with open(f'texts/chn_ver/chapter{i}.txt', 'w', encoding='utf-8') as f:
+      f.write(decoded_text)
 
 
 def cal_weights(text):
@@ -27,9 +52,19 @@ def cal_weights(text):
 def gen_dict():
   with open('texts/original2.txt', 'r', encoding='utf-8') as f:
     text = f.read()
+  with open('spider/vocabs.txt', 'r') as f:
+    vocabs = f.readlines()
   weights_dict = cal_weights(text)
-  sorted_s = sorted(weights_dict.items(), key=lambda d: d[1], reverse=True)
-  print(sorted_s[:10])
+  words = sorted(weights_dict.items(), key=lambda d: d[1], reverse=True)
+  chn2eng_dict = {}
+  eng2chn_dict = {}
+  for chn, eng in zip(words, vocabs):
+    chn2eng_dict[chn[0]] = eng.strip()
+    eng2chn_dict[eng.strip()] = chn[0]
+  with open('dicts/chn2eng', 'wb') as f:
+    pickle.dump(chn2eng_dict, f)
+  with open('dicts/eng2chn', 'wb') as f:
+    pickle.dump(eng2chn_dict, f)
 
 
 def split2chaps():
@@ -45,4 +80,4 @@ def split2chaps():
 
 
 if __name__ == '__main__':
-  gen_dict()
+  decoding()
